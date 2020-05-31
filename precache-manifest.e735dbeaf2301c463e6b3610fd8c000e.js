accce09deaa63a97bc00fa7f399c76abecff4032
lipskye <lipskye@sina.com>
2020-05-31 23:09:13 +0800

    'deploy'

---
+++ precache-manifest.e735dbeaf2301c463e6b3610fd8c000e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb04b466cf013566d35",
+    "url": "/abc/css/app.5a6bb6b4.css"
+  },
+  {
+    "revision": "0f8c2c10db5dec1d2d9a",
+    "url": "/abc/css/chunk-vendors.b6741065.css"
+  },
+  {
+    "revision": "c3b3b895852ca7ab0663",
+    "url": "/abc/css/helper/duty.59e04a8c.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/abc/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/abc/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d6379db28ee961d4f9d7a3c3fd3e5d9f",
+    "url": "/abc/img/chengshuai.d6379db2.jpg"
+  },
+  {
+    "revision": "7ce7ecbe355435e9b60ebfebbe4a76ce",
+    "url": "/abc/img/lengxiaofeng.7ce7ecbe.jpg"
+  },
+  {
+    "revision": "de7a123b2e4961533d793d25a367ddcf",
+    "url": "/abc/img/liuna.de7a123b.jpg"
+  },
+  {
+    "revision": "c3510f25398328864cddcf5e560a2de7",
+    "url": "/abc/img/star.c3510f25.png"
+  },
+  {
+    "revision": "5f2fb54e508fd4b43e369a76846c7d42",
+    "url": "/abc/img/sunyalin.5f2fb54e.jpg"
+  },
+  {
+    "revision": "89c99e0b695a7a1a757d7b9683c7d919",
+    "url": "/abc/img/yaoxiaoyan.89c99e0b.jpg"
+  },
+  {
+    "revision": "ce0f6090a98cb682853f82f92346d851",
+    "url": "/abc/index.html"
+  },
+  {
+    "revision": "9eb04b466cf013566d35",
+    "url": "/abc/js/app.511c05ff.js"
+  },
+  {
+    "revision": "0f8c2c10db5dec1d2d9a",
+    "url": "/abc/js/chunk-vendors.b00b15c9.js"
+  },
+  {
+    "revision": "c3b3b895852ca7ab0663",
+    "url": "/abc/js/helper/duty.db7bd6fa.js"
+  },
+  {
+    "revision": "46bf87a36a5c3ee8e40b7b182406034a",
+    "url": "/abc/manifest.json"
+  },
+  {
+    "revision": "b17ec8212ab95c27e9e444a0af85c0c1",
+    "url": "/abc/media/music.b17ec821.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/abc/robots.txt"
+  }
+]);
